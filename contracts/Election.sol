66877883fad470d9431e81b46c032373ab134aa8
Grey Rabbitty <GreyRabbitty@gmail.com>
2023-09-13 15:51:34 +0200

    truffle - smart contract versions updated
    Candidates updated without city and zipcode

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity ^0.8.7;
 
 contract Election {
     // Model a Candidate
@@ -6,8 +6,6 @@ contract Election {
         uint256 id;
         string name;
         uint256 voteCount;
-        string city;
-        string zipCode;
     }
 
     // Store accounts that have voted
@@ -22,16 +20,16 @@ contract Election {
     event votedEvent(uint256 indexed _candidateId);
 
     constructor() public {
-        addCandidate("AKP - Adalet ve Kalk?nma Parti");
-        addCandidate("CHP - Cumhuriyet Halk Partisi");
-        addCandidate("?Y? - ?yi Parti");
-        addCandidate("MHP - Milliyet�i Hareket Partisi");
-        addCandidate("HDP - Halklar?n Demokratik Partisi");
+        addCandidate("AKP - Adalet ve Kalkınma Parti");
+        addCandidate("CHP - Cumhuriyet Halk P,artisi");
+        addCandidate("İYİ - İyi Parti");
+        addCandidate("MHP - Milliyetçi Hareket Partisi");
+        addCandidate("HDP - Halkların Demokratik Partisi");
     }
 
     function addCandidate(string memory _name) private {
         candidatesCount++;
-        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0, _city, _zipCode);
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
     function vote(uint256 _candidateId) public {
--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity ^0.8.7;
 
 contract SimpleStorage {
   uint storedData;
